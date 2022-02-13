cd0135d793e3677342dcc80cc387b97535369591
Abhishek Tomar <itsabhi.at@gmail.com>
2022-02-13 16:40:52 +0530

    library/updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-// imports here 
-import React , {useState} from 'react';
-// import styles 
-import './styles/app.scss';
-// import components 
-import Player from './components/Player';
-import Songs from './components/Songs';
+// imports here
+import React, { useState } from "react";
+// import styles
+import "./styles/app.scss";
+// import components
+import Player from "./components/Player";
+import Songs from "./components/Songs";
 //import utils
 import data from "./util/data";
+import Library from "./Library";
 
 function App() {
   //state
-  const [songs,setSongs]=useState(data());
-  const [currentSong,setCurrentSong]=useState(songs[0]);
-  const [isPlaying,setIsPlaying]=useState(false);
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Songs currentSong={currentSong} />
-      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
+      <Library songs={songs} />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,6 +4,7 @@ import {
   faPlay,
   faAngleLeft,
   faAngleRight,
+  faPause,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
@@ -30,8 +31,8 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   };
   //   state
   const [songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null,
+    currentTime: 0,
+    duration: 0,
   });
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
@@ -57,7 +58,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
           onClick={playSongHandler}
           className="play"
           size="2x"
-          icon={faPlay}
+          icon={isPlaying ? faPause : faPlay}
         />
         <FontAwesomeIcon
           className="skip-forward"
